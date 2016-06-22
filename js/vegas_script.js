4f4c7ba07564bcf8ca7daa64737fea1c946adb8b
skyrob <skyrob@hotmail.com>
2016-06-22 19:33:20 +0200

    Add files via upload

---
+++ js/vegas_script.js
@@ -0,0 +1,27 @@
+
+$(function() {
+$('body').vegas({
+timer: true,
+transitionDuration: 2000,
+overlay: 'js/vegas/overlays/02.png',
+slides: [
+{ src: 'pictures/slide1.jpg' },
+{ src: 'pictures/slide2.jpg' },
+{ src: 'pictures/slide3.jpg' },
+{ src: 'pictures/slide4.jpg' },
+{ src: 'pictures/slide5.jpg' },
+{ src: 'pictures/slide6.jpg' },
+{ src: 'pictures/slide7.jpg' },
+{ src: 'pictures/slide8.jpg' },
+{ src: 'pictures/slide9.jpg' },
+{ src: 'pictures/slide10.jpg' },
+{ src: 'pictures/slide11.jpg' },
+{ src: 'pictures/slide12.jpg' },
+{ src: 'pictures/slide13.jpg' },
+{ src: 'pictures/slide14.jpg' },
+{ src: 'pictures/slide15.jpg' },
+{ src: 'pictures/slide16.jpg' },
+{ src: 'pictures/slide17.jpg' }
+]
+});
+});
